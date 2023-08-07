3b1b1bb5dd1c883ab73497c656179d743e095366
Gilian <giliankeulens@happyhorizon.com>
2023-08-07 14:02:43 +0200

    Initial commit for Mollie plugin for GeeksCoreLibrary order process.

---
+++ GeeksCoreLibrary.Modules.Payments.Mollie/Models/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace GeeksCoreLibrary.Modules.Payments.Mollie.Models;
+
+public class Constants
+{
+    public const string WebhookInvoiceNumberProperty = "invoice_number";
+
+    public const string MollieApiKeyLiveProperty = "mollieapikeylive";
+
+    public const string MollieApiKeyTestProperty = "mollieapikeytest";
+}
---
+++ GeeksCoreLibrary.Modules.Payments.Mollie/Models/MollieSettingsModel.cs
@@ -0,0 +1,11 @@
+﻿using GeeksCoreLibrary.Components.OrderProcess.Models;
+
+namespace GeeksCoreLibrary.Modules.Payments.Mollie.Models;
+
+public class MollieSettingsModel : PaymentServiceProviderSettingsModel
+{
+    /// <summary>
+    /// Gets or sets the API key for the current environment.
+    /// </summary>
+    public string ApiKey { get; set; }
+}
